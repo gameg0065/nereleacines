b6a3d72fedc2b291e50bd35c5acab4e2e46bc2fc
Tomas <gameg0065@gmail.com>
2022-12-02 11:25:00 +0200

    Neo4j

---
+++ Neo4j/Neo4j/Program.cs
@@ -0,0 +1,60 @@
+﻿using Neo4j.Driver;
+
+namespace Neo4j.ConsoleApp
+{
+    class Program : IDisposable
+    {
+        private bool _disposed;
+        private readonly IDriver _driver;
+
+        ~Program() => Dispose(false);
+
+        private Program(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        private async Task PrintGreeting(string message)
+        {
+            await using var session = _driver.AsyncSession();
+            var greeting = await session.ExecuteWriteAsync(async tx =>
+            {
+                var result = await tx.RunAsync("CREATE (a:Greeting) " +
+                                    "SET a.message = $message " +
+                                    "RETURN a.message + ', from node ' + id(a)",
+                    new {message});
+                
+                return result.SingleAsync().Result[0].As<string>();
+            });
+            Console.WriteLine(greeting);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                _driver?.Dispose();
+            }
+
+            _disposed = true;
+        }
+        
+        static async Task Main(string[] args)
+        {
+            using var greeter = new Program("bolt://localhost:7687", "neo4j", "test");
+            
+            await greeter.PrintGreeting("hello, world");
+            Console.WriteLine("test");
+        }
+
+    }
+}
---
+++ Neo4j/README.md
@@ -0,0 +1,22 @@
+# Neo4j
+
+## Task
+
+Parašyti paprastą programą įgyvendinančią sritį tinkamą grafų duomenų bazėms.
+1. Sumodeliuokite bent kelias esybes su savybėmis.
+2. Pademonstruokite prasmingas užklausas: 2.1. Surasti esybes pagal savybę (pvz. rasti asmenį pagal asmens kodą, rasti banko sąskaitą pagal numerį). 2.2. Surasti esybes pagal ryšį (pvz. banko sąskaitas priklausančias asmeniui, banko korteles susietas su konkretaus asmens sąskaitomis). 2.3. Surasti esybes susietas giliais sąryšiais (pvz. draugų draugus, visus kelius tarp Vilniaus ir Klaipėdos; visus autobusus kuriais galima nuvažiuoti iš stotelės X į stotelę Y). 2.4. Surasti trumpiausią kelią (pvz. surasti trumpiausią kelią tarp Vilniaus ir Klaipėdos; surasti pigiausią būdą konvertuoti iš valiutos X į valiutą Y, kuomet turima visų bankų konversijos informacija ir optimalus būdas, gali būti atlikti kelis žingsnius). 2.5. Agreguojami duomenys (pvz. kaip 2.4, tik surasti kelio ilgį ar konversijos kainą). Nenaudokite trumpiausio kelio.
+
+Paprastumo dėlei, būkite pasiruošę testinius duomenis. Programa turėtų leisti atlikti užklausas (tarkime įvedamas miestas X, miestas Y ir suplanuojamas kelias tarp jų).
+
+## Software
+
+* Install [.net core 6.0](https://dotnet.microsoft.com/en-us/download)
+* Install [Neo4j on dotnet](https://neo4j.com/developer/dotnet/)
+* Install [Neo4j on docker](https://neo4j.com/developer/docker-run-neo4j/)
+
+## Build and run
+
+* dotnet build
+* dotnet run
+* 
+
